4878d99d181121e025f4c0d731363bd65e9af5ec
shreyaas5 <ssachdeva1_be24@thapar.edu>
2025-10-15 13:06:58 +0530

    LeetCode 160

---
+++ assignment 5/aq1.cpp
@@ -0,0 +1,21 @@
+//LeetCode 160
+
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return NULL;
+        ListNode* pA = headA;
+        ListNode* pB = headB;
+        while (pA != pB) {
+            pA = pA ? pA->next : headB;
+            pB = pB ? pB->next : headA;
+        }
+        return pA;
+    }
+};
