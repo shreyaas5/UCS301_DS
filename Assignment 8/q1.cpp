80529bbc2500f8f35dec09e9bf0babd8de87e1be
shreyaas5 <ssachdeva1_be24@thapar.edu>
2025-11-18 23:01:16 +0530

    Binary Tree traversals

--- Assignment 8/q1.cpp
+++ Assignment 8/q1.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+using namespace std;
 
+struct Node {
+    int key;
+    Node* left;
+    Node* right;
+
+    Node(int k) : key(k), left(NULL), right(NULL) {}
+};
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->key << " ";
+    inorder(root->right);
+}
+
+void preorder(Node* root) {
+    if (!root) return;
+    cout << root->key << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root) {
+    if (!root) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->key << " ";
+}
