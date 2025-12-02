64d914a5d016330c5bfae83544916367f1ddd2ff
shreyaas5 <ssachdeva1_be24@thapar.edu>
2025-12-03 01:37:07 +0530

    Binary Tree Traversals

--- Assignment 8/q1.cpp
+++ Assignment 8/q1.cpp
@@ -31,3 +31,33 @@ void postorder(Node* root){
     postorder(root->right);
     cout << root->key << " ";
 }
+
+int main(){
+    Node* x1 = new Node(1);
+    Node* x2 = new Node(3);
+    Node* x3 = new Node(2);
+    Node* x4 = new Node(4);
+    Node* x5= new Node(7);
+    Node* x6 = new Node(8);
+    
+    x1->left = x2;
+    x1->right = x3;
+    x2->left = x4;
+    x2->right = x5;
+    x3->right = x6;
+
+    cout << "Inorder: ";
+    inorder(x1);
+    cout << "\n";
+
+    cout << "Preorder: ";
+    preorder(x1);
+    cout << "\n";
+
+    cout << "Postorder: ";
+    postorder(x1);
+    cout << "\n";
+
+    return 0;
+
+}
